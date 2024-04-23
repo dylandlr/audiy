ce06a849a060a4003144fbf5882827cf4b2ce960
Dylan De La Rosa <delarosadyl@gmail.com>
2024-04-22 22:48:48 -0500

    flask project version one

---
+++ audiy-app/app/app.py
@@ -0,0 +1,2 @@
+# OAUTH2_DOMAIN=dev-dyl.us.auth0.com
+# OAUTH2_Redirect_URI=http://localhost:3000/callback
---
+++ audiy-app/app/run.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('../audiy/audiy/audiy-app')
+from __init__ import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
